8a6c0abfe1e742fb52fabbf84562d4ed09f6f350
Alexander zegarra de la cruz <>
2024-09-15 18:28:49 -0500

    AlexanderZEGARRA: realizado el calculo del valor de inventario

---
+++ src/casoinventario/calculovalorinventario2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casoinventario;
+
+/**
+ *
+ * @author zegar
+ */
+public class calculovalorinventario2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double costo4 = 10.00;
+        double stock4 = 15;
+        double totalCosto4 = stock4 * costo4;
+     
+       
+       System.out.println("total costo 4" + totalCosto4);
+    }
+    
+}
