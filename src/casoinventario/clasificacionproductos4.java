67b4956edf84998af1ce286083c28b16e335353d
alexander zegarra de la cruz <>
2024-09-15 17:40:51 -0500

    alexander zegarra: realizo el calculo de clasificacion de productos

---
+++ src/casoinventario/clasificacionproductos4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casoinventario;
+
+/**
+ *
+ * @author domingoram
+ */
+public class clasificacionproductos4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int tipo1=1;
+        String mensajeCat1 = "";
+        
+        mensajeCat1 = switch (tipo1) {
+            case 1 -> "Producto electronico. Revisar garantia.";
+            case 2 -> "Producto alimenticio. Revisar fecha de caducidad.";
+            case 3 -> "Producto de ropa. Revisar tallas disponibles.";
+            default -> "Categoria no valida.";
+        };
+    }
+    
+}
