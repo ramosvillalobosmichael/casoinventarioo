abfea41ab5a8c3c90fb6690cb1693d1655be00e2
john bryan Adams Saldivar <>
2024-09-15 19:20:36 -0500

    john bryan Adams Saldivar : realizo la toma de desiciones

---
+++ src/casoinventario/tomadedesiciones5.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package casoinventario;
+
+/**
+ *
+ * @author zegar
+ */
+public class tomadedesiciones5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double totalCosto5=600;
+        
+        if (totalCosto5 > 500) {
+            System.out.println("Atención: Se recomienda reducir el inventario ya que el valor total supera los 500 soles.");
+        }
+    }
+    
+}
