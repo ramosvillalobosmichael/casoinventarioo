4db32391b5b585c2cc7d47be1dcfb7852965b7ce
ramosvillalobosmichael <>
2024-09-15 19:00:10 -0500

    ramos_villalobos_michael: ha realizado el proceso de leer la entarda de datos

--- src/casoinventario/entradadatos1.java
+++ src/casoinventario/entradadatos1.java
@@ -35,18 +35,18 @@ public class entradadatos1 {
         Scanner scanner=new Scanner(System.in);//declarado scanner para poder leer los datos
         
         System.out.println("Ingrese el primer producto");
-        String item1 = scanner.nextLine();
+        String item2 = scanner.nextLine();
         System.out.println("Ingrese cantidad del primer producto");
-        int stock1 = scanner.nextInt();
+        int stock2 = scanner.nextInt();
         System.out.println("Ingrese el costo del primer producto");
-        double costo1 = scanner.nextDouble();
+        double costo2 = scanner.nextDouble();
         System.out.println("Ingrese la categoria del primer producto (1.- Electronicos, 2.- Alimentos, 3.- Ropa):");
-        int tipo1 = scanner.nextInt();
+        int tipo2 = scanner.nextInt();
         
-        System.out.println("producto1:\t"+item1);
-        System.out.println("cantidad de producto1:\t"+stock1);
-        System.out.println("costo de producto1:\t"+costo1);
-        System.out.println("tipo de producto:\t"+tipo1);
+        System.out.println("producto1:\t"+item2);
+        System.out.println("cantidad de producto1:\t"+stock2);
+        System.out.println("costo de producto1:\t"+costo2);
+        System.out.println("tipo de producto:\t"+tipo2);
     }
     
 }
